0d1514332e966275493b1a9b6e3788e8f8e3bd9f
Carlos Maldonado <camv29@gmail.com>
2019-10-04 15:45:21 -0700

    Initial commit

---
+++ src/components/Alumnos.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Alumnos"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <b-table hover :items="results" :busy.sync="isBusy"></b-table>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios';
+
+export default {
+
+  name: 'home',
+  data() {
+    return {
+      isBusy:  false,
+      search: "",
+      results: {}
+    }
+  },
+
+  mounted(){
+    this.getUser();
+  },
+
+  methods: {
+    getUser() {
+      axios
+              .get("http://localhost:3000/users")
+              .then(response => (this.results = response.data))
+              .catch(error => alert(error));
+    }
+  },
+
+  components: {
+    HelloWorld
+  }
+}
+</script>
